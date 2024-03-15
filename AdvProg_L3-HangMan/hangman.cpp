c67c4f82487c3b141b7d56fd19c1fd8bf1d3fb66
GiangDurian <gianglessin2@gmail.com>
2024-03-15 17:58:13 +0700

    new update

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -121,7 +121,7 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
 void updateEnteredChars(const char ch, string& chars){
     // TODO: append the character ch is in end of the text chars
     //if(!isCharInWord(ch,chars))  
-        chars += (" " + ch);
+        chars += ch;
 }
 
 /***
